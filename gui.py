8d2c8bb4e796694cf1c27c3be9dca12652fe2fa0
Berteh <36950+berteh@users.noreply.github.com>
2023-04-07 16:51:52 +0200

    + LU translation

--- gui.py
+++ gui.py
@@ -53,8 +53,12 @@ sein Gewicht wird an der Kasse anhand des Strichcodes auf dem Etikett abgezogen"
         'status': ""
     },
     'LU' : {
-        'print': "Imprimer",
-        'instructions': "",
+        'print': "Dréckt Är\nEtikett aus",
+        'instructions': """1. Setzt Ären eidele Container mat sengem Deckel op der Skala
+2. Dréckt eng Etikett dat säin eidelt Gewiicht enthält
+3. Pescht dëse Label un de Behälter op enger zougänglecher Plaz
+4. Fëllt Äre Behälter mat 1 Typ vu Produkt, säi Gewiicht gëtt
+an der Kees ofgezunn dank dem Barcode un der Etikett""",
         'status': ""
     },
     'PT' : {
