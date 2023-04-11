bd1020bbd56c10285aa11ab2231cd72bcc540d20
berteh <36950+berteh@users.noreply.github.com>
2023-04-12 00:25:20 +0200

    test for transparent buttons

--- gui.py
+++ gui.py
@@ -131,6 +131,25 @@ class App:
         
 
         #flags
+        """ opt use tkinter.ttk themes for flags
+         s = ttk.Style()
+        s.configure('Wild.TButton',
+            background='black',
+            foreground='white',
+            highlightthickness='20',
+            font=('Helvetica', 18, 'bold'))
+        s.map('Wild.TButton',
+            foreground=[('disabled', 'yellow'),
+                        ('pressed', 'red'),
+                        ('active', 'blue')],
+            background=[('disabled', 'magenta'),
+                        ('pressed', '!focus', 'cyan'),
+                        ('active', 'green')],
+            highlightcolor=[('focus', 'green'),
+                            ('!focus', 'red')],
+            relief=[('pressed', 'groove'),
+                    ('!pressed', 'ridge')])
+        """
         luO = Image.open(PICSDIR+"LU.png")
         luR = resize_image(luO, [width*0.05, height*0.05])
         self.luI = ImageTk.PhotoImage(luR)
