1a6902604e5e148f742bbf871b4f729a0266c613
Berteh <36950+berteh@users.noreply.github.com>
2023-05-17 15:21:42 +0200

    fix path

--- ubuntu/install_as_user_service.sh
+++ ubuntu/install_as_user_service.sh
@@ -4,7 +4,7 @@
 mkdir -p $HOME/.config/systemd/user/
 while read line; do   # Replace all instances on line of script-dir with $PWD
    echo ${line//script-dir/$PWD}
-done < TareKiosk.service > $HOME/.config/systemd/user/TareKiosk.service
+done < ubuntu/TareKiosk.service > $HOME/.config/systemd/user/TareKiosk.service
 systemctl --user daemon-reload
 systemctl --user enable TareKiosk.service
 #systemctl --user start TareKiosk.service
